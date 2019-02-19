33c3ac4b2b782b764a15917c98d753ce600e96ae
jmacek <kuba.macek@o2.pl>
2019-02-19 20:40:49 +0100

    run flask app

---
+++ blog-app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Home Page"
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
