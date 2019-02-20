58426b6e556fbdfe67a3a61d698da39eb6cd484d
jmacek <kuba.macek@o2.pl>
2019-02-20 21:17:09 +0100

    added templates + easy bootstrap

--- blog-app.py
+++ blog-app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Post 1',
+        'content': 'Post 1 content',
+        'date': 'May 3, 1997'
+    },
+    {
+        'author': 'Betty Doe',
+        'title': 'Post 2',
+        'content': 'Post 2 content',
+        'date': 'May 5, 1997'
+    }
+]
+
 @app.route("/")
+@app.route("/home/")
 def hello():
-    return "Home Page"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "About Page"
+    return render_template('about.html', title='about')
 
 if __name__ == '__main__':
     app.run(debug=True)
