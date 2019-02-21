4e94a89241fc8edc64494dac1c0287a28fec41d4
jmacek <kuba.macek@o2.pl>
2019-02-21 20:35:25 +0100

    register and login pages created

--- blog-app.py
+++ blog-app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9f4eb91852838b381ed8eacfeb528eae'
 
 posts = [
     {
@@ -17,13 +21,32 @@ posts = [
 ]
 
 @app.route("/")
-@app.route("/home/")
-def hello():
+@app.route("/home")
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='about')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed!. Check your credentials!', 'danger')
+    return render_template('login.html', title='login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
