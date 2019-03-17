c5d57aeb2a5a623e3af817c78eaf43217afdde82
jmacek <kuba.macek@o2.pl>
2019-03-17 20:05:49 +0100

    handling error pages

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -24,8 +24,10 @@ def create_app(config_class=Config):
     from blog_app.users.routes import users
     from blog_app.posts.routes import posts
     from blog_app.main.routes import main
+    from blog_app.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
